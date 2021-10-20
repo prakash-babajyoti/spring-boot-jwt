082c530314a922cb2cd77a678d17d61aac73d762
Prakash Babajyoti <prakash.babajyoti@gmail.com>
2021-10-20 21:54:23 +0530

    Initial Commit

---
+++ src/test/java/edu/prakash/springbootjwt/SpringBootJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.prakash.springbootjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
